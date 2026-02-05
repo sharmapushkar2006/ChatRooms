52bf9721fa7d2c03d0b9f127c1eb66738e7e5f38
sharmapushkar2006 <spushkar2205@gmail.com>
2026-02-06 03:13:04 +0530

    Update script.js

--- login/script.js
+++ login/script.js
@@ -1,9 +1,10 @@
 var token = window.location.search;
 var savetoken = token.replace("?authid=","");   
 if(savetoken==""){
-    //location.href="https://pushkarsharma2006.github.io/login?redirect=about:blank"
+    location.href="https://sharmapushkar2006.github.io/login?c=https://sharmapushkar2006.github.io/chatrooms/login";
 }
 else{
     localStorage.setItem("authid",savetoken);
     location.href="../index.html"
+
 }
